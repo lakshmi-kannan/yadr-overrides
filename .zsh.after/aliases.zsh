64783fdb645c7233f29e993482b284894a257337
Lakshmi Kannan <lakshmi.kannan@rackspace.com>
2014-06-25 21:58:34 -0700

    Initial checkin

---
+++ .zsh.after/aliases.zsh
@@ -0,0 +1,3 @@
+alias vmssh='ssh 172.168.50.50'
+#function mntvm() { mkdir -p ~/vm/ && sudo mount -t nfs -o timeo=900,retrans=3,proto=tcp,resvport,nolocks,locallocks,intr,soft,wsize=32768,rsize=3276 "$*":/home/lakshmi ~/vm/ ; }
+function mntvm() { mkdir -p ~/vm/ && sudo mount -t nfs -o timeo=900,retrans=3,proto=tcp,resvport,nolocks,locallocks,intr,soft,wsize=32768,rsize=3276 172.168.50.50:/home/lakshmi/src ~/vm/}
